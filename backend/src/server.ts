c1038da38d765d538ca31440c8006da55466d1d4
AdamKoda2306 <adam.koda2306@gmail.com>
2026-07-05 02:44:28 +0530

    backend code for starting the server

---
+++ backend/src/server.ts
@@ -0,0 +1,27 @@
+import { Server } from "socket.io";
+import http from "http";
+import enableLogging, { attachDashboard } from "logsave-hub";
+import { env } from "./config/env";
+import { connectDB } from "./config/db";
+import app from "./app";
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+attachDashboard(io);
+
+enableLogging({
+  override: true,
+  outDir: "./logs",
+  retention: false
+});
+
+async function startServer() {
+  await connectDB();
+  server.listen(env.PORT, () => {
+    console.log(`[SERVER] Running on http://localhost:${env.PORT}`);
+    console.log(`[SERVER] Environment: ${env.NODE_ENV}`);
+  });
+}
+
+startServer();
